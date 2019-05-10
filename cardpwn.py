024554978945b7f099778e8cc1295c8a05edcafc
Hacker Destination <atushar.473@gmail.com>
2019-05-10 14:02:59 +0530

    More Sources Added

--- cardpwn.py
+++ cardpwn.py
@@ -10,7 +10,7 @@ G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m' # white
 
-version = '1.0'
+version = '1.1'
 
 def banner():
 	os.system('clear')
@@ -88,8 +88,8 @@ def network():
 
 try:
 	banner()
-	#network()
-	#updater()
+	network()
+	updater()
 	cardpwn()
 except KeyboardInterrupt:
 	print ('\n' + R + '[!]' + R + ' Keyboard Interrupt.' + W)
