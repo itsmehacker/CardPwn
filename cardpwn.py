cfb40f8c5d25a539934e16b578829a6b7837e8fc
itsmehacker <atushar940@gmail.com>
2019-05-11 01:42:07 +0530

    Version 1.4

--- cardpwn.py
+++ cardpwn.py
@@ -25,27 +25,10 @@ def banner():
 	print(G + '[>] ' + R + 'Created by : ' + W + 'Hacker Destination')
 	print(G + '[>] ' + R + 'Version : ' + W + version)
 
-def updater():
-	print (G + '[+]' + R + ' Checking for updates...' + W + '\n')
-	updated_version = requests.get('https://raw.githubusercontent.com/itsmehacker/cardpwn/master/version.txt', timeout = 5)
-	updated_version = updated_version.text.split(' ')[1]
-	updated_version = updated_version.strip()
-	if updated_version != version:
-		print (G + '[!]' + R + ' A New Version is Available : ' + W + updated_version)
-		ans = input(G + '[!]' + R + ' Update ? [y/n] : ' + W)
-		if ans == 'y':
-			print ('\n' + G + '[+]' + R + ' Updating...' + '\n' + W)
-			subprocess.check_output(['git', 'reset', '--hard', 'origin/master'])
-			subprocess.check_output(['git', 'pull'])
-			print (G + '[+]' + R + ' Script Updated...Please Execute Again...' + W)
-			exit()
-	else:
-		print (G + '[+]' + R + ' Script is up-to-date...' + '\n' + W)
-
 def cardpwn():
 	urls = []
 	qlist = []
-	website = []
+	total_url = []
 	paste_sites = ['cl1p.net', 'dpaste', 'dumpz.org', 'hastebin', 'ideone', 'pastebin', 'pw.fabian-fingerle.de','gist.github.com','https://www.heypasteit.com/','ivpaste.com','mysticpaste.com','paste.org.ru','paste2.org','sebsauvage.net/paste/','slexy.org','squadedit.com','wklej.se','textsnip.com']
 	card = input(G + '[+] ' + R +'Enter Card No. -> ' + W)
 	try:
@@ -63,15 +46,16 @@ def cardpwn():
 				for site in paste_sites:
 					if '{}'.format(site) in item:
 						print(G + '[+] ' + C + item + W)
+						total_url.append(item)
 
 		else:
 			print('\n' + R + '[!] ' + G + 'Invaild Card Number' + W + '\n')
 			return cardpwn()
-			total = len(urls)
-			if total == 0:
-				print (R + '[-] No Open Leaks for this Card Number Found.' + W + '\n')
-			else:
-				print('\n' + G + '[+]' + R + ' Total Dumps Found : ' + W + str(total) + '\n')
+		total = len(total_url)
+		if total == 0:
+			print (R + '[-] No Open Leaks for this Card Number Found.' + W + '\n')
+		else:
+			print('\n' + G + '[+]' + R + ' Total Dumps Found : ' + W + str(total) + '\n')
 
 	except ValueError:
 		print('\n' + R + '[!] Invaild Card Number Entered...' + W + '\n')
@@ -89,7 +73,6 @@ def network():
 try:
 	banner()
 	network()
-	updater()
 	cardpwn()
 except KeyboardInterrupt:
 	print ('\n' + R + '[!]' + R + ' Keyboard Interrupt.' + W)
